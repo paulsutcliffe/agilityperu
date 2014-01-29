9f7ec9b71af8aedfdceddfa9ce4950c32d9d1ba9
Paul Sutcliffe <paul@kosmyka.com>
2014-01-29 17:58:51 -0500

    gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agilityperu::Application.config.secret_key_base = '22f474fa7cbfa5c3fc81ff07ea717a813967651cdc657359e5ebe0e0ed791258de49e164a93ba87699add891f987f010ad0ed13a8499d969ce26dbf4a3818319'
