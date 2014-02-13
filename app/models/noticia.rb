071110fc4611038c9a620726cad5c8ab7c7223ea
Paul Sutcliffe <paul@kosmyka.com>
2014-02-13 01:18:00 -0500

    detalles

--- app/models/noticia.rb
+++ app/models/noticia.rb
@@ -1,6 +1,8 @@
 class Noticia < ActiveRecord::Base
   mount_uploader :foto, FotoUploader
 
+  default_scope  { order("fecha DESC") }
+
   def noticia_anterior
     self.class.first(:conditions => ["id < ?", id], :order => "id desc")
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Agilityperu::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['jquery.countdown/jquery.plugin.min.js', 'jquery.countdown/jquery.countdown.css', 'jquery.countdown/jquery.countdown-es.js', 'jquery.countdown/jquery.countdown.min.js']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
