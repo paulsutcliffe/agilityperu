a9d07a3373dd2f43364d81abeddf9af10187393e
adrielpinzas <adriel@kosmyka.com>
2014-03-06 15:40:28 -0500

    corregido el mailer

--- app/mailers/contacto_mailer.rb
+++ app/mailers/contacto_mailer.rb
@@ -2,6 +2,6 @@ class ContactoMailer < ActionMailer::Base
   default from: "info@kosmyka.com"
   def mensaje_recibido(mensaje)
     @mensaje = mensaje
-    mail(:to => "web@agilityperu2014.com", :subject => "Mensajes desde la web", :replay_to => mensaje.email, :from => "info@agilityperu2014.com")
+    mail(:to => "info@agilityperu2014.com", :subject => "Mensajes desde la web", :replay_to => mensaje.email, :from => "info@agilityperu2014.com")
   end
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => 'mail.agilityperu2014.com',
   :port                 => 587,
   :domain               => 'agilityperu2014.com',
-  :user_name            => 'web@agilityperu2014.com',
-  :password             => 'agility99',
+  :user_name            => 'info@agilityperu2014.com',
+  :password             => 'kTf^Mc2Y',
   :authentication       => 'plain',
   :enable_starttls_auto => false 
 }
