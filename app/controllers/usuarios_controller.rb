8fd367aa7189c23fe5b0ec279828fc761f740267
adrielpinzas <adriel@kosmyka.com>
2014-03-05 19:29:47 -0500

    index de usuarios

---
+++ app/controllers/usuarios_controller.rb
@@ -0,0 +1,13 @@
+class UsuariosController < InheritedResources::Base
+  before_filter :authenticate_admin!
+  def update
+    if params[:usuario][:password].blank?
+      params[:usuario].delete(:password)
+      params[:usuario].delete(:password_confirmation)
+    end
+  end
+  def destroy
+    destroy!(notice: "El usuario se a eliminado con éxito") { usuarios_path }
+  end
+end
+
