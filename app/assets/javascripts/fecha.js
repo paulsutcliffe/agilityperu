119db5f6c0d126a4199a86eb4cc6da850989a1cd
Paul Sutcliffe <paul@kosmyka.com>
2014-03-24 12:52:23 -0500

    update

--- app/assets/javascripts/fecha.js
+++ app/assets/javascripts/fecha.js
@@ -8,11 +8,12 @@ ready = function() {
        interval: 2000,
         wrap: true
         });
-    $(function () {
+
+    function() {
       	var austDay = new Date();
       	austDay = new Date(2014, 4-1, 03);
       	$('#contador').countdown({until: austDay, format: 'dHM'});
-      });
+    };
 }
-  $(document).ready(ready);
+$(document).ready(ready);
 $(document).on('page:load', ready);
